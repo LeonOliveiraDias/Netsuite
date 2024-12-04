54eaa75eedc941eff616615328e24045bcd64661
Dias, Leon <ldias@netsuite.com>
2024-12-04 00:11:18 -0300

    Init Commit without Connected Services

---
+++ Netsuite/SuiteTalkCourse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Netsuite.NetSuiteService;
+
+namespace Netsuite
+{
+    class SuiteTalkCourse
+    {
+        static void Main(string[] args)
+        {
+            // Display the number of command line arguments.
+            Console.WriteLine(args.Length);
+
+
+            Netsuite.NetSuiteService.NetSuitePortTypeClient _client = new NetSuitePortTypeClient();
+            Netsuite.NetSuiteService.TokenPassport tokenPassport = new TokenPassport();
+        }
+    }
+}
