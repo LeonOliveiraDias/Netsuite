f325ff3a0a1d7f2a673a723d1ad261d3b410a83b
Dias, Leon <ldias@netsuite.com>
2024-12-05 01:07:42 -0300

    Adding web service reference; Getting employee information;

--- Netsuite/SuiteTalkCourse.cs
+++ Netsuite/SuiteTalkCourse.cs
@@ -1,22 +1,71 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Netsuite.NetSuiteService;
+using Netsuite.com.netsuite.webservices;
+
 
 namespace Netsuite
 {
     class SuiteTalkCourse
     {
+        public NetSuiteService _service;
+        private Preferences _preferences;
+        private SearchPreferences _searchPreferences;
+
+        public SuiteTalkCourse() { 
+            _service = new NetSuiteService();
+
+            _service.Url = "https://td2967825.suitetalk.api.netsuite.com";
+
+            SetTokenPassport();
+            SetApplicationInfo();
+            SetPreferences();
+            GetEmployee();
+        }
+
+
+        private void SetApplicationInfo() {
+            _service.applicationInfo = new ApplicationInfo
+            {
+                applicationId = ""
+            };
+        }
+
+        private void SetTokenPassport() {
+            _service.tokenPassport = new TokenPassport
+            {
+                consumerKey = "",
+                token = "",
+                Role = "",
+                account = "",
+            };
+        }
+
+        private void SetPreferences() {
+            _service.preferences = new Preferences { };
+            _service.searchPreferences = new SearchPreferences { };
+        }
+
+        private void GetEmployee() {
+            RecordRef employeeRef = new RecordRef
+            {
+                internalId = "312",
+                type = RecordType.employee,
+                typeSpecified = true,
+            };
+
+            Console.WriteLine(employeeRef);
+
+            _service.get(employeeRef);
+
+            Console.WriteLine("After customer get");
+        }
+
         static void Main(string[] args)
         {
-            // Display the number of command line arguments.
-            Console.WriteLine(args.Length);
+            SuiteTalkCourse suiteTalkCourse = new SuiteTalkCourse();
+
+            suiteTalkCourse.GetEmployee();
 
 
-            Netsuite.NetSuiteService.NetSuitePortTypeClient _client = new NetSuitePortTypeClient();
-            Netsuite.NetSuiteService.TokenPassport tokenPassport = new TokenPassport();
         }
     }
 }
